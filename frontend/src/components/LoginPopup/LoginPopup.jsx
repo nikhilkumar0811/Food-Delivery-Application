b8aebe804f7913dd150f6bc751f67275a1841f46
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-08-13 23:28:57 +0530

    Adds the codebase for the food delivery app

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
+
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login");
+
+  return (
+    <div className="login-Popup">
+      <form action="" className="login-Popup-container">
+        <div className="login-Popup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt="close"
+          />
+        </div>
+        <div className="login-Popup-inputs">
+          {currState === "Sign Up" && (
+            <input type="text" placeholder="Your name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="password" required />
+        </div>
+        <button type="submit">
+          {currState === "Sign Up" ? "Create account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span
+              onClick={() => setCurrState("Sign Up")}
+              style={{ cursor: "pointer", color: "blue" }}
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span
+              onClick={() => setCurrState("Login")}
+              style={{ cursor: "pointer", color: "blue" }}
+            >
+              Login here
+            </span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
